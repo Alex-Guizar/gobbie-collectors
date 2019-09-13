9edf2edd63572fbd43e636e7acf069a488552731
Alex Guizar <alexguizar90@gmail.com>
2019-09-12 17:18:56 -0700

    Putting together index.js file

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render(
+  <App />, document.querySelector('#root')
+);
