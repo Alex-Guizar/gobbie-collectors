547ce9fe7fb4052cc1331c605582c7452c9f410c
Alex Guizar <alexguizar90@gmail.com>
2019-11-20 18:41:02 -0800

    Set up basic footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react';
+// Bootstrap Components
+import Container from 'react-bootstrap/Container';
 
 const Footer = () => {
   return (
     <footer>
-      <div className="container-fluid text-center">
-        Footer
-      </div>
+      <Container fluid className="text-center">
+        <div>Gobbie Collectors</div>
+        <div><a href="#">@GobbieCollectors</a> <a href="#">Github</a></div>
+        <div>Disclaimers</div>
+        <div>&copy; Gobbie Collectors</div>
+        <div>Final Fantasy XIV &copy; 2010 - 2019 SQUARE ENIX CO., LTD. All Rights Reserved.</div>
+      </Container>
     </footer>
   );
 };
