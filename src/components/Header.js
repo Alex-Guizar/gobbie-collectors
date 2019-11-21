79e712b3a63f29a1cb643402d8165c3df2194226
Alex Guizar <alexguizar90@gmail.com>
2019-11-20 18:23:33 -0800

    Set up basic header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,37 @@
 import React from 'react';
+// Bootstrap Components
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
 const Header = () => {
   return (
     <header>
-      <nav className="navbar navbar-dark bg-dark text-white">
-        Gobbie Collectors
-      </nav>
+      <Navbar bg="dark" variant="dark" expand="md">
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/logo192.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+          Gobbie Collectors
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="framework_drop" />
+        <Navbar.Collapse id="framework_drop">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#minons">Minions</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
     </header>
   );
 };
