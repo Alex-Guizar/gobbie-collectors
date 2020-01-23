8becba0f3b965836d8958b113bea67c700b176c6
Alex Guizar <alexguizar90@gmail.com>
2020-01-22 17:33:15 -0800

    Playing with FFXIV API

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,27 @@ import Footer from './Footer';
 import SearchPage from './search/SearchPage';
 import ItemPage from './ItemPage';
 
+const XIVAPI = require('xivapi-js');
+const xiv = new XIVAPI();
+
+xiv.search("", {indexes: 'companion'}).then((response) => {
+  // do something with the response
+  console.log(response);
+}).catch((error) => {
+  // do something with the error
+  console.log(error);
+});
+
+const searchCompanions = () => {
+  xiv.search("wind-up").then((response) => {
+    // do something with the response
+    console.log(response);
+  }).catch((error) => {
+    // do something with the error
+    console.log(error);
+  });
+}
+
 const HomePage = () => {
   return <div>Home Page</div>;
 };
